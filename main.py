ac7dbeb5ddac12a096d7f94ebf8650fbd7888382
MohammedNajih <102456093+MohammedNajih@users.noreply.github.com>
2022-03-31 11:23:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def masg(call):
 		file='done.txt'
 		for Whisper in open(file,'r').read().splitlines():
 			id=str(Whisper.split('\n')[0])
-			link = requests.get(f'http://coins-php-ahmedbasrah97282154200.codeanyapp.com/coins.php?oid={id}&submit=submit').text
+			link = requests.get(f'https://cin.oussamavip.repl.co/?oid={id}&submit=submit').text
 			if 'coins":"' in link:
 				coin = link.split('"')[0]
 				ok+=1
