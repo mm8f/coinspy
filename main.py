f2daeedc6664db01b633d9a56f0afc7fb949fb55
MohammedNajih <102456093+MohammedNajih@users.noreply.github.com>
2022-04-03 05:27:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -23,9 +23,11 @@ def boten(message):
     
     A = types.InlineKeyboardButton(text ="COINS INSTAUP|فحص نقاط انستا اب ", callback_data="F1")
     
+    E = types.InlineKeyboardButton(text ="حذف", callback_data="F1") 
+    
     M = types.InlineKeyboardButton('المطور', url='https://t.me/t_4gi')
     
-    mas.add(A,M)
+    mas.add(A,E,M)
     
     bot.send_message(message.chat.id, f"-BOT CHECKER COINS INSTA APP \n -DEVLOPER MOHAMMED ALMUSWI \n -CHANEEL : @ONCLIK @ONCLCK \n -بوت فحص نقاط انستا اب \n -برمجه محمد الموسوي \n -اختر الطلب وسيتم الفحص حظا موفا ❤️",reply_markup=mas)
     
@@ -42,7 +44,8 @@ def masg(call):
 		
 		A = types.InlineKeyboardButton(text ="COINS INSTAUP|فحص نقاط انستا اب ", callback_data="F1")
 
-		
+		E = types.InlineKeyboardButton(text ="حذف", callback_data="F2") 
+
 		M = types.InlineKeyboardButton('المطور', url='https://t.me/t_4gi')
 		
 		M = types.InlineKeyboardButton('المطور', url='https://t.me/t_4gi')
@@ -68,7 +71,7 @@ def masg(call):
 			link = f'https://xq.oussamasalim.repl.co/?id={id}&proxy={proxy}'
 			headers = {"Host": "impracticalrightshoutcast.mrrobotreal1.repl.co","Connection": "keep-alive","Cache-Control": "max-age=0","sec-ch-ua": 'Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',"sec-ch-ua-mobile": "?1","sec-ch-ua-platform": "Android","X-Chrome-offline": "persist=0 reason=reload","Upgrade-Insecure-Requests": "1","User-Agent": str(generate_user_agent()),"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9","Sec-Fetch-Site": "none","Sec-Fetch-Mode": "navigate","Sec-Fetch-User": "?1","Sec-Fetch-Dest": "document","Accept-Encoding": "gzip","Accept-Language": "ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7", "X-Forwarded-For": proxy }
 			response = requests.get(link,headers=headers,proxies=proxies).text
-			if ('{"coins":null}}')in response:
+			if ('instaup.developers@gmail.com')in response:
 				cp+=1
 			elif ('{"status":"Successful","message":"Sucess!","return":{"coins":"') in response:
 				ok+=1
@@ -86,7 +89,8 @@ def masg(call):
 				mas.add(A,E,B,R,M)
 				bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text="جاري الفحص النقاط ",reply_markup=mas)
 				
-
+	elif call.data =="F2":
+		os.system('rm -rf done.txt')
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
     json_string = request.get_data().decode("utf-8")
